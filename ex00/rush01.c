bad1d31a5a266b972de09c09eb32d6e7c9c0b5a9
Fryt Os <frytos@Fryts-Mac-mini.local>
2024-02-11 10:21:20 +0100

    adds criteria for rush 01

---
+++ ex00/rush01.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush01.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlaine <jlaine@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/10 18:58:33 by jlaine            #+#    #+#             */
+/*   Updated: 2024/02/11 10:15:48 by argrouss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	rush(int x, int y);
+void	ft_putchar(char c);
+void	display(int x, int y, int c, int l);
+
+void	rush(int x, int y)
+{
+	int		l;
+	int		c;
+
+	if (x > 0 && y > 0)
+	{
+		l = 0;
+		c = 0;
+		while (l < y)
+		{
+			c = 0;
+			while (c < x)
+			{
+				display(x, y, c, l);
+				c++;
+			}
+			ft_putchar(10);
+			l++;
+		}
+	}
+}
+
+void	display(int x, int y, int c, int l)
+{
+	if ((l == 0 && c == 0) || (x - c == 1 && y - l == 1))
+	{
+		ft_putchar('/');
+	}
+	else if ((l == 0 && x - c == 1) || (c == 0 && y - l == 1))
+	{
+		ft_putchar(92);
+	}
+	else if (l == 0 || c == 0 || x - c == 1 || y - l == 1)
+	{
+		ft_putchar('*');
+	}
+	else
+	{
+		ft_putchar(' ');
+	}
+}
+
